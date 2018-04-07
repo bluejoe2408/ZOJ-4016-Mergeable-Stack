028f31afaa00374689c7cb63025f6b5f3f8e9a07
bluejoe2408 <3150102408@zju.edu.cn>
2018-04-08 00:35:39 +0800

    commit

--- main.cpp
+++ main.cpp
@@ -2,9 +2,55 @@
 #include <list>
 using namespace std;
 
+list<int> l[300003];
+
+void push(int s, int v)
+{
+    l[s].push_back(v);
+}
+
+void pop(int s)
+{
+    list<int>::iterator i;
+    if(l[s].empty()) {cout << "EMPTY" << "\n";return;}
+    i = l[s].end();
+    i--;
+    cout << *i;
+}
+
+void move(int s, int t)
+{
+    l[s].splice(l[s].end(),l[t]);
+}
+
 int main() {
-    int t;
-    list<int> l;
-    cin >> t;
+    int T;
+    cin >> T;
+    for (;T--;)
+    {
+        int n,q;
+        for(int i = 1; i<=n; i++) l[i].clear();
+        cin >> n >> q;
+        for(;q--;)
+        {
+            int op;
+            cin >> op;
+            if (op == 1){
+                int s, v;
+                cin >> s >> v;
+                push(s,v);
+            }
+            else if (op == 2){
+                int s;
+                cin >> s;
+                pop(s);
+            }
+            else if (op == 3){
+                int s, t;
+                cin >> s >> t;
+                move(s, t);
+            }
+        }
+    }
     return 0;
 }
